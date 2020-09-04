58c02c151e1b697221322727fd48eeb9e3a9818d
Andrew Yu <30904629+YuDrew@users.noreply.github.com>
2020-09-04 04:16:46 -0500

    Configure SectionList

--- App.js
+++ App.js
@@ -5,13 +5,16 @@ import {
   View,
   Button,
   SafeAreaView,
+  SectionList,
+  ScrollView
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  centered: {
+  Event: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'yellow',
   },
   spacedRow: {
     flexDirection: 'row',
@@ -23,33 +26,69 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  sectionHeader: {
+    fontSize: 16,
+    backgroundColor: "#fff"
+  },
+  listHeader: {
+    fontSize: 25,
+    backgroundColor: "#f0f"
+  },
 });
 
-/*
-const Event = (summary, id, description, start, end) => {
-  return(
+const DATA = [
+  {
+    title: "Some Date",
+    data: [
+      {
+        summary: "Event One",
+        description: "Stuff happens in event One",
+        start: {
+          dateTime: new Date().getHours() + ":" + new Date().getMinutes(),
+          timeZone: "CST"
+        },
+        end: {
+          dateTime: (new Date().getHours()+ 1) + ":" + new Date().getMinutes(),
+          timeZone: "CST"
+        }
+      },
+      {
+        summary: "Event Two",
+        description: "Stuff happens in event Two",
+        start: {
+          dateTime: (new Date().getHours()+3) + ":" + new Date().getMinutes(),
+          timeZone: "CST"
+        },
+        end: {
+          dateTime: (new Date().getHours()+4) + ":" + new Date().getMinutes(),
+          timeZone: "CST"
+        }
+      }
+    ]
+  },
+  {
+    title: "Another Date",
+    data: []
+  }
+]
 
-  ); 
-}
-*/
-
-const EventTemplateFunction = () => {
+const Event = ({summary, description, start, end}) => {
   //Declare state variables
   const [isTracking, setIsTracking] = useState(false);
   //ToDo: Add in a Timer in order to update time elapsed regularly
   //ToDo: Add in a Time Elapsed state variable
 
   return(
-    <View style={styles.centered}>
+    <View style={styles.Event}>
       <View style={{flexDirection: 'row'}}>
         <View style={{flexDirection: 'column', flex: 5}}>
           <View style={styles.spacedRow}>
-            <Text style={styles.eventTitle}> Title of Event </Text>
-            <Text style={{alignSelf: 'flex-end'}}> Elapsed: 00:00:00 </Text>
+            <Text style={styles.eventTitle}> {summary} </Text>
           </View>
           <View style={styles.spacedRow}> 
-            <Text> Start: 00:00 XM </Text> 
-            <Text> End: 00:00 XM </Text>
+            <Text> Start: {start.dateTime} XM </Text> 
+            <Text> End: {end.dateTime} XM </Text>
+            <Text style={{alignSelf: 'flex-end'}}> Elapsed: 00:00:00 </Text>
           </View>
         </View>
         <Button
@@ -57,7 +96,7 @@ const EventTemplateFunction = () => {
           onPress={() => {
             setIsTracking(!isTracking);
           }}
-          title={isTracking ? "stop" : "start"}
+          title={isTracking ? "⏹" : "▶️"}
         />
       </View>
     </View>
@@ -66,8 +105,18 @@ const EventTemplateFunction = () => {
 
 const App = () => {
   return(
-    <SafeAreaView style={{flex: 1}}>
-      <EventTemplateFunction/>
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        ListHeaderComponent={() => <Text style={styles.listHeader}> Some Header </Text>}
+        sections= {DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => 
+          <Event summary={item.summary} description={item.description} start={item.start} end={item.end} />
+        }
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.sectionHeader}>{title}</Text> 
+        )}
+      />
     </SafeAreaView>
   );
 }
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # CalenTime
  Timetracking using the Google Calendar API and Firebase.
 ## Current Functionality
-* Not much, just some basic formatting
+* Not much, just some basic formatting.
 * Connected app to Firebase, though it's not used yet.
 ## To-Dos
 * Break up components into separate files for readability
-* Set up Google Login Authorizations
+* Set up Google Sign In and OAuth
 * Connect to Google Calendar API to populate event information (titles, start times, and end times)
 * Set up Firebase Realtime Database to track time elapsed
 * Create detail views to track performance on each event (comparing planned activity to true activity)
@@ -14,3 +14,5 @@
 * Firebase Setup: https://rnfirebase.io/
 * gitignore Template: https://github.com/facebook/react-native/blob/master/template/_gitignore
 * React Native Docs: https://reactnative.dev/
+* Calendar Auth: https://developers.google.com/calendar/auth
+* Google OAuth: https://developers.google.com/identity/protocols/oauth2/
